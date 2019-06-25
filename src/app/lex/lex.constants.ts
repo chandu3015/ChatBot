1b21097b847b99fa4f39c6930cecff6370d88653
chandu3015 <chandu3015@gmail.com>
2019-06-25 04:04:57 -0600

    done excluding lexruntime

---
+++ src/app/lex/lex.constants.ts
@@ -0,0 +1,9 @@
+export  class LexConstants {
+  public static  Bot_Alias = '$LATEST';
+  public static BOT_NAME = 'RestBotTest';
+  public static USER_ID = 'user';
+  public static AccessKey_Id = 'AKIAQCKD2NNITBSL4N54';
+  public  static SecretAccess_Key = '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';
+  public  static Region = 'us-east-1';
+
+}
