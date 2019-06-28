10c76b69ed46a48878c95459445fd843d27fc568
chandu3015 <40059088+chandu3015@users.noreply.github.com>
2019-06-28 17:03:39 -0600

    removing key

--- src/app/lex/lex.constants.ts
+++ src/app/lex/lex.constants.ts
@@ -2,8 +2,8 @@ export  class LexConstants {
   public static  Bot_Alias = '$LATEST';
   public static BOT_NAME = 'RestBotTest';
   public static USER_ID = 'user';
-  public static AccessKey_Id = 'AKIAQCKD2NNITBSL4N54';
-  public  static SecretAccess_Key = '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';
+  public static AccessKey_Id = '';
+  public  static SecretAccess_Key = '';
   public  static Region = 'us-east-1';
 
 }
